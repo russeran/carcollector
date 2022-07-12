89ba35f8e27654a0e291657f2b2b8431e857370b
russeran <rusen.seran@gmail.com>
2022-07-12 12:30:27 -0700

    Create Delete Update CBVs

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,7 @@
+from black import List
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+# from django.views.generic import DetailView
 from . models import Car
 
 # Create your views here.
@@ -13,6 +16,26 @@ def cars_index(request):
     cars = Car.objects.all()
     return render(request, 'cars/index.html', {'cars': cars})
 
+
+
 def cars_detail(request, car_id):
     car = Car.objects.get(id=car_id)
     return render(request, 'cars/detail.html', {'car': car})
+
+# class CarDetail(DetailView):
+#     model = Car
+#     template_name = 'cars/detail.html'
+  
+
+
+class CarCreate(CreateView):
+    model = Car
+    fields = '__all__'
+    
+class CarUpdate(UpdateView):
+    model = Car
+    fields = '__all__'
+
+class CarDelete(DeleteView):
+    model = Car
+    success_url = '/cars/'        
