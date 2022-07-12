74185f78e1229cc27525e51eb46abc424e723bc8
russeran <rusen.seran@gmail.com>
2022-07-11 21:19:55 -0700

    index and details views, css

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    name = models.CharField(max_length=255)
+    make = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
+    year = models.IntegerField()
+    price = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} - {self.make}"
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from . models import Car
 
 # Create your views here.
+#define home view
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+    
+def cars_index(request):
+    cars = Car.objects.all()
+    return render(request, 'cars/index.html', {'cars': cars})
+
+def cars_detail(request, car_id):
+    car = Car.objects.get(id=car_id)
+    return render(request, 'cars/detail.html', {'car': car})
